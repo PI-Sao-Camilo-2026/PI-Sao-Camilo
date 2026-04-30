f4a9004302e5ace306d0d8956f2044624bc1a59c
Rodrigo <rodr.2102@gmail.com>
2026-04-30 08:49:47 -0300

    imagens adicionadas

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,19 +1,24 @@
 import "../css/Login.css";
+import logo from "../assets/logo.png";
+import silhueta from "../assets/silhueta.png";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const navigate = useNavigate(); // ✅ TEM QUE FICAR AQUI
+  const navigate = useNavigate();
 
   return (
     <div className="login-page">
       <div className="phone-screen">
         <header className="login-header">
+          <img src={logo} alt="Logo São Camilo" />
           <h1>SÃO CAMILO</h1>
           <p>Nutri - Esportiva</p>
         </header>
 
         <main className="login-main">
-          <div className="sport-icon">⚽</div>
+          <div className="atleta-box">
+            <img className="atleta" src={silhueta} alt="Silhueta Atleta" />
+          </div>
 
           <h2>Bem-vindo!</h2>
           <p className="subtitle">Acesse sua conta para continuar</p>
